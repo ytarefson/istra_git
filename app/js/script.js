a68b6f2a3d6f3b102f19a7b93f874c0db18a745c
ytarefson <ytarefson@gmail.com>
2017-10-20 09:07:14 +0400

    v 1.1
    Version v 1.1 getting ready for catalog injection

---
+++ app/js/script.js
@@ -0,0 +1,86 @@
+$(document).ready( function() {
+  var products = [];
+  products = [ 
+  {
+    id:1, 
+    name: "htc",
+    href: "../img/sample/phone1.png",
+    price: 1000,
+    desc: "Description of htc"
+  }
+  ];
+/*  {
+    id:2, 
+    name: "samsung",
+    href: "../img/sample/phone2.png",
+    price: 1000,
+    desc: "Description of samsung"
+  },
+  {
+    id:3, 
+    name: "apple",
+    href: "../img/sample/phone3.png",
+    price: 1000,
+    desc: "Description of apple"
+  },
+  {
+    id:4, 
+    name: "huawei",
+    href: "../img/sample/phone4.png",
+    price: 1000,
+    desc: "Description of huawei"
+  },
+  {
+    id:5, 
+    name: "nokia",
+    href: "../img/sample/phone5.png",
+    price: 1000,
+    desc: "Description of nokia"
+  },
+  {
+    id:6, 
+    name: "motorolla",
+    href: "../img/sample/phone6.png",
+    price: 1000,
+    desc: "Description of motorolla"
+  },
+  {
+    id:7, 
+    name: "lg",
+    href: "../img/sample/phone7.png",
+    price: 1000,
+    desc: "Description of lg"
+  },
+  {
+    id:8, 
+    name: "asus",
+    href: "../img/sample/phone8.png",
+    price: 1000,
+    desc: "Description of asus"
+  },
+  {
+    id:9, 
+    name: "lenovo",
+    href: "../img/sample/phone9.png",
+    price: 1000,
+    desc: "Description of lenovo"
+  },
+  {
+    id:10, 
+    name: "fly",
+    href: "../img/sample/phone10.png",
+    price: 1000,
+    desc: "Description of fly"
+  },
+  {
+    id:11, 
+    name: "xiaomi",
+    href: "../img/sample/phone11.png",
+    price: 1000,
+    desc: "Description of xiaomi"
+  }
+];
+*/
+console.log(products.length);
+});
+
